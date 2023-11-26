7a183d61e8ee78015362e616984cea13fdf1c411
badrxd <badrxdxd@gmail.com>
2023-11-26 05:44:09 +0100

    add details in doday component

--- components/main/Main.jsx
+++ components/main/Main.jsx
@@ -13,7 +13,7 @@ function Main() {
   return (
     <div className="flex gap-5">
       <div className="w-4/6 rounded-2xl p-6 flex flex-col gap-5">
-        <Today />
+        <Today weather={weather} temperature={temperature} />
         <TodayForecast temperature={temperature} weather={weather} />
         <AirCondition
           day={weather.forecast.forecastday[0].day}
--- components/main/weather/Today.jsx
+++ components/main/weather/Today.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Image from "next/image";
+import { getByUnit } from "@/utils/GetByUnits";
+import { useSelector } from "react-redux";
 
-function Today() {
-  return <div className="bg-[#202C3C] rounded-2xl p-6">Today</div>;
+function Today({ weather, temperature }) {
+  const { location, current } = weather;
+  const { temp } = useSelector((store) => store.settings);
+  return (
+    <div className="bg-[#202C3C] rounded-2xl p-6">
+      <div>
+        <h1>{location.name}</h1>
+        <p>{current.temp_c}</p>
+        <p>
+          {getByUnit(current, temperature, temp)}
+          {temperature[temp].symbole}
+        </p>
+      </div>
+      <div>
+        <Image
+          src={`http:${current.condition.icon}`}
+          width={200}
+          height={200}
+          alt="Picture of the author"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Today;
