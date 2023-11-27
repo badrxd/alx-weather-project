fc27b9d266f38626968f146ca39105eab57f93cc
assouljawad <38533122+assouljawad@users.noreply.github.com>
2023-11-27 15:14:37 +0100

    fix settings buttons

--- components/main/Settings.jsx
+++ components/main/Settings.jsx
@@ -13,7 +13,7 @@ function Settings() {
       return (
         <div key={i} className="w-1/2">
           <div
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded flex justify-center items-center"
             onClick={() => {
               dispatch(changeSettings({ i: i, type: type }));
             }}
