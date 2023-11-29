32639b9078628743d6d0e644492582b40a1f9cf3
badrxd <badrxdxd@gmail.com>
2023-11-29 06:59:42 +0100

    make the data be saved in browser

--- app/Provider.jsx
+++ app/Provider.jsx
@@ -1,10 +1,17 @@
 "use client";
 import React from "react";
-import { store } from "@/redux/store";
+import { store, persistor } from "@/redux/store";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 function ReduxProvider({ children }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
 
 export default ReduxProvider;
--- redux/store.js
+++ redux/store.js
@@ -1,12 +1,40 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./features/settings";
 import locationSlice from "./features/location";
 import weatherSlice from "./features/weather";
+import storage from "redux-persist/lib/storage";
+import { persistReducer, persistStore } from "redux-persist";
+import thunk from "redux-thunk";
+import sessionStorage from "redux-persist/es/storage/session";
+
+const persistConfig = {
+  key: "root",
+  sessionStorage,
+  storage,
+};
+const rootReducer = combineReducers({
+  settings: counterReducer,
+  location: locationSlice,
+  weather: weatherSlice,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    settings: counterReducer,
-    location: locationSlice,
-    weather: weatherSlice,
-  },
+  reducer: persistedReducer,
+  middleware: [thunk],
 });
+export const persistor = persistStore(store);
+
+// import { configureStore } from "@reduxjs/toolkit";
+// import counterReducer from "./features/settings";
+// import locationSlice from "./features/location";
+// import weatherSlice from "./features/weather";
+
+// export const store = configureStore({
+//   reducer: {
+//     settings: counterReducer,
+//     location: locationSlice,
+//     weather: weatherSlice,
+//   },
+// });
